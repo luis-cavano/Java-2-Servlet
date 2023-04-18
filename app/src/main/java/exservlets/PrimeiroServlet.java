d0af74fa3dff760aaecc4e3637122dc5cfb9c250
luis-cavano <luissevilhadev@gmail.com>
2023-04-18 15:32:13 -0300

    add: PrimeiroServlet.java

---
+++ app/src/main/java/exservlets/PrimeiroServlet.java
@@ -0,0 +1,18 @@
+package exservlets;
+
+import java.io.IOException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name = "IndexServlet", urlPatterns = "/")
+public class PrimeiroServlet  extends HttpServlet{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+        throws ServletException, IOException {
+            String n = request.getParameter("nome"); 
+            //response.getWriter().print("Olá "+n);
+            request.getRequestDispatcher("/index.jsp");
+    }
+}
